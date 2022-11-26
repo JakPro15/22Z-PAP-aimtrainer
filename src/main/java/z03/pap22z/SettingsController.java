08277de2d9f75502010178da4b9f34ffa6ce0645
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2022-11-27 00:08:34 +0100

    Implemented basic working settings slider ui

--- src/main/java/z03/pap22z/SettingsController.java
+++ src/main/java/z03/pap22z/SettingsController.java
@@ -1,11 +1,31 @@
 package z03.pap22z;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 import java.io.IOException;
 
 public class SettingsController extends z03.pap22z.SceneController {
+    public void initialize() {
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> source, Number oldValue, Number newValue) {
+                volumeValueLabel.textProperty().setValue(String.format("%d%%", (int)volumeSlider.getValue()));
+            }
+        });
+    }
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private Label volumeValueLabel;
+
     @FXML
     protected void handleExit(ActionEvent event) {
         try {
@@ -13,5 +33,15 @@ public class SettingsController extends z03.pap22z.SceneController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @FXML
+    void handleSave(ActionEvent event) {
+        System.out.println("Save pressed");
+    }
+
+    @FXML
+    void volumeSliderChanged(MouseEvent event) {
+        System.out.println("Slider changed");
     }
 }
