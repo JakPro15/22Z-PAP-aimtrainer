66e011458d16e43fe96fc07353fbd7bfb2ea045c
Kamil Michalak <01169209@pw.edu.pl>
2022-12-06 00:37:51 +0100

    Added basic game menu control

--- src/main/java/z03/pap22z/GameMenuController.java
+++ src/main/java/z03/pap22z/GameMenuController.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+
 
 public class GameMenuController extends z03.pap22z.SceneController {
-    @FXML
-    private ListView<String> game_list;
-
     @FXML
     protected void handleExit(ActionEvent event) {
         try {
@@ -17,5 +16,31 @@ public class GameMenuController extends z03.pap22z.SceneController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @FXML
+    private TextArea text_hint;
+
+    @FXML
+    private Button hint_1;
+
+    @FXML
+    private Button hint_2;
+
+    @FXML
+    private Button hint_3;
+
+    @FXML
+    protected void handleHint(ActionEvent event) {
+        if (event.getSource() == hint_1) {
+            text_hint.setText("How to play game number 1");
+        }
+        if (event.getSource() == hint_2) {
+            text_hint.setText("How to play game number 2. I specially put here longer text to see what happens.");
+        }
+        if (event.getSource() == hint_3) {
+            text_hint.setText("How to play game number 3. I like studying IT :) ");
+        }
+        text_hint.setVisible(true);
     }
 }
