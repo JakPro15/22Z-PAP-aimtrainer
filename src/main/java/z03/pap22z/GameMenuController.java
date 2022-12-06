95c6d88b982d6d032b9f397aa28eb38d4dcba4b3
kpalucki <91991025+Chr1sps@users.noreply.github.com>
2022-12-06 16:57:14 +0100

    Added messages displayed in TextArea for games that are yet to be implemented.

--- src/main/java/z03/pap22z/GameMenuController.java
+++ src/main/java/z03/pap22z/GameMenuController.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.TextArea;
 
 public class GameMenuController extends z03.pap22z.SceneController {
     @FXML
@@ -31,6 +30,12 @@ public class GameMenuController extends z03.pap22z.SceneController {
     private Button hint_3;
 
     @FXML
+    protected void handleToBeImplemented(ActionEvent event) {
+        text_hint.setText("Game to be implemented!");
+        text_hint.setVisible(true);
+    }
+
+    @FXML
     protected void handleHint(ActionEvent event) {
         if (event.getSource() == hint_1) {
             text_hint.setText("How to play game number 1");
@@ -42,5 +47,14 @@ public class GameMenuController extends z03.pap22z.SceneController {
             text_hint.setText("How to play game number 3. I like studying IT :) ");
         }
         text_hint.setVisible(true);
+    }
+
+    @FXML
+    protected void switchToAimSniper(ActionEvent event) {
+        try {
+            switchToScene(event, "AimSniper");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
