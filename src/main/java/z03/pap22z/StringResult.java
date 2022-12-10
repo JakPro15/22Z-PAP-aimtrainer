42675fc25abccb58f871004b83c2f544c56f25a4
Jakub Proboszcz <jakub.proboszcz.stud@pw.edu.pl>
2022-12-10 14:13:43 +0100

    Fixed accuracy rounding bug in results

--- src/main/java/z03/pap22z/StringResult.java
+++ src/main/java/z03/pap22z/StringResult.java
@@ -11,6 +11,11 @@ public class StringResult {
     private String gameTime, user, gameType, speed, length, score, accuracy;
     private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
+    /**
+     * Creates a StringResult containing all fields of the given result converted to strings.
+     * Only id is not converted, but saved as an integer.
+     * @param result
+     */
     public StringResult(Result result) {
         id = result.getId();
         gameTime = result.getGameTime().format(dateFormatter);
