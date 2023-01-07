9a4d70f5241c3cfc6a28fe7c127fe8e1bd5e153a
kpalucki <91991025+Chr1sps@users.noreply.github.com>
2023-01-07 02:35:45 +0100

    Removed clearing .log files from bash scripts.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -4,14 +4,12 @@
 cd $(dirname "$0")
 source Scripts/mysql_variables.sh
 
-if ! which java >> /dev/null
-then
+if ! which java >>/dev/null; then
     echo "Java needs to be installed to launch the program."
     exit 1
 fi
 
-if [ ! -f $mysqld ];
-then
+if [ ! -f $mysqld ]; then
     echo "MySQL server needs to be installed to launch the program."
     exit 1
 fi
@@ -20,8 +18,8 @@ fi
 $mysqld --defaults-file="$HOME/pap22Z_z03_mysql/mysql_config.cnf" &
 sleep 2
 # set up user and database
-$mysql --host=localhost --socket="$HOME/pap22Z_z03_mysql/mysqld.sock" -u root < Scripts/setup_user.sql
-$mysql --host=localhost --socket="$HOME/pap22Z_z03_mysql/mysqld.sock" -u pap22Z_z03 -ppap.2022.PAP < Scripts/setup_database.sql
+$mysql --host=localhost --socket="$HOME/pap22Z_z03_mysql/mysqld.sock" -u root <Scripts/setup_user.sql
+$mysql --host=localhost --socket="$HOME/pap22Z_z03_mysql/mysqld.sock" -u pap22Z_z03 -ppap.2022.PAP <Scripts/setup_database.sql
 
 mvn package
 rm dependency-reduced-pom.xml
@@ -30,4 +28,3 @@ mv AimTrainer-1.0.0.jar AimTrainer.jar
 
 java --module-path Others/openjfx/lib --add-modules=javafx.controls,javafx.fxml --add-opens java.base/java.lang=ALL-UNNAMED -cp AimTrainer.jar z03.pap22z.App
 $mysqladmin --host=localhost --socket="$HOME/pap22Z_z03_mysql/mysqld.sock" -u root shutdown
-rm *.log
