4ff0b6827b365e9e7c42f5504e8548575249c39d
Kamil Michalak <01169209@pw.edu.pl>
2023-01-09 23:36:16 +0100

    removed unnecessary method in GameManager ; tried to add button extensibility

--- src/main/java/z03/pap22z/controllers/GameMenuController.java
+++ src/main/java/z03/pap22z/controllers/GameMenuController.java
@@ -32,18 +32,10 @@ public class GameMenuController extends z03.pap22z.controllers.SceneController {
     private Button hint_3;
 
     @FXML
-    protected void handleToBeImplemented(ActionEvent event) {
-        MusicManager.playButtonSound();
-        text_hint.setText("Game to be implemented!");
-        text_hint.setVisible(true);
-    }
-
-    @FXML
     protected void handleHint(ActionEvent event) {
         MusicManager.playButtonSound();
         if (event.getSource() == hint_1) {
-            text_hint.setText(
-                    "AimSniper: hit the target as many times as you can while it teleports on every successful click you make!");
+            text_hint.setText("AimSniper: hit the target as many times as you can while it teleports on every successful click you make!");
         }
         if (event.getSource() == hint_2) {
             text_hint.setText("KeyboardWarrior: press the buttons on the keyboard in the correct order as soon as they appear on the line!");
